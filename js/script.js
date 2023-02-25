8064a9e0a6446ed0b95263159b29083d3e81a006
Tremendousturtle1 <125144740+Tremendousturtle1@users.noreply.github.com>
2023-02-25 11:04:20 -0600

    images added

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const myImages = [
+    {
+        src: "/images/suits.jpg",
+        description: "two kids in suits.",
+        alt: "caterpillar"
+    },
+    {
+        src: "/images/kayack.jpg",
+        description: "Two kids in a kayack.",
+        alt: "chipmunk"
+    },
+];
+
+const showcaseImageElement = document.getElementById('showcase-image');
+const showcaseImageDescription = document.getElementById('showcase-description');
+const nextButton = document.getElementById('next-button');
+const prevButton = document.getElementById('prev-button');
+
+let currentIndex = 0;
+
+showcaseImageElement.src = myImages[currentIndex].src
+showcaseImageElement.alt = myImages[currentIndex].alt
+showcaseImageDescription.innerText = myImages[currentIndex].description
+
+function changeMyImage(e){
+    if (e.target.id === nextButton.id) {
+        if (currentIndex < myImages.length -1) {
+            currentIndex += 1
+        } else {
+            currentIndex = 0
+        } 
+    }   
+    if (e.target.id === prevButton.id) {
+        if (currentIndex > 0) {
+            currentIndex -= 1
+        } else {
+            currentIndex = myImages.length - 1
+        }
+    }
+    
+    showcaseImageElement.src = myImages[currentIndex].src
+    showcaseImageElement.alt = myImages[currentIndex].alt
+    showcaseImageDescription.innerText = myImages[currentIndex].description
+}
+
+nextButton.onclick = changeMyImage;
+prevButton.onclick = changeMyImage;
